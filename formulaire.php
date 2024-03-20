829e4e8993b045030152394f3e032233de16af77
alxbld237 <alxbld05@gmail.com>
2024-03-20 14:19:07 +0100

    Troisieme commit

--- formulaire.php
+++ formulaire.php
@@ -38,6 +38,13 @@
 	else {
 		echo "Le fichier est présent" . "<br>";
 	}
+	
+	if (empty($_POST['classe'])) { // Champs de la Classe
+echo "Erreur de classe" . "<br>";
+}
+else {
+echo "Classe conforme" . "<br>";
+}
 
 
 
--- tableau3.php
+++ tableau3.php
@@ -1,7 +1,3 @@
 <?php
-$marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
-$i=0;
-foreach ($elem as $marque) {
-	echo "Vente de : " . $elem . "<br/>";
-}
+
 ?>
